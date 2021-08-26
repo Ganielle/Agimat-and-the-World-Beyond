1b74ab5da2fbbde7b5b429f8f3baa69e4a0a3288
paoloandal <paoloa@realtair.com>
2021-08-26 14:53:01 +0800

    8-26-2021 2:52
    Added basic AI patrol and chase

---
+++ Assets/001 - AgimatAndTheWorldBeyond/002 - Script/005 - AI Mobs/EnemyWallChecker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWallChecker : MonoBehaviour
+{
+    //global references
+    [SerializeField] private GameObject mainPlayer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("ran into wall");
+            if (transform.parent.GetComponent<AIPatrol>().hasClimbingFeature)
+            {
+                transform.parent.GetComponent<AIPatrol>().isPatrolling = false;
+                transform.parent.GetComponent<AIPatrol>().ClimbUp();
+            }               
+            else
+                transform.parent.GetComponent<AIPatrol>().Flip();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (transform.parent.GetComponent<AIPatrol>().hasClimbingFeature)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                Debug.Log("done climbing");
+                transform.parent.GetComponent<AIPatrol>().anim.SetBool("isClimbing", false);
+                transform.parent.GetComponent<AIPatrol>().isPatrolling = true;
+                transform.parent.GetComponent<AIPatrol>().rb.gravityScale = 1f;
+            }    
+                
+        }       
+    }
+}
