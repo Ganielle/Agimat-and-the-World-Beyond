5767734df550a81751488ba6d40ff0af9ae47e68
paoloandal <paoloa@realtair.com>
2021-08-28 14:04:08 +0800

    Fixed AI chase and climbing system

--- Assets/001 - AgimatAndTheWorldBeyond/002 - Script/005 - AI Mobs/AIPatrol.cs
+++ Assets/001 - AgimatAndTheWorldBeyond/002 - Script/005 - AI Mobs/AIPatrol.cs
@@ -6,32 +6,49 @@ using UnityEngine;
 public class AIPatrol : MonoBehaviour
 {
     //global references
-    [SerializeField] private GameObject mainPlayer;
+    [SerializeField] private GameObject detectedPlayer;
 
-    //mob states and capabilities
+    [Header("Mob Data")]
     [SerializeField] private float patrolDistance;
     [SerializeField] private float patrolSpeed;
     [SerializeField] private float climbSpeed;
-    [SerializeField] public bool hasClimbingFeature;
+    
     [SerializeField] private float chaseThreshold;
-    [ReadOnly] [SerializeField] public bool isPatrolling;   //opposite of patrolling is chasing
-    [ReadOnly] [SerializeField] public bool isChasing;
-    [ReadOnly] [SerializeField] public bool isAttacking;
-    private Vector3 startingPoint;
+    [ReadOnly] [SerializeField] public bool isPatrolling; 
+    [ReadOnly] [SerializeField] private bool isAgro;
+    [ReadOnly] [SerializeField] private bool isChasing;
+    [ReadOnly] [SerializeField] private bool isTouchingWall;
+    [ReadOnly] [SerializeField] private bool isSteppingOnLedge;
+    [ReadOnly] [SerializeField] private bool isFacingRight;
+    private Vector2 startingPoint;
     [ReadOnly][SerializeField] private float distanceTravelled;
 
+    [Header("Features")]
+    [SerializeField] public bool willChasePlayer;
+    [SerializeField] public bool hasClimbingFeature;
+    [SerializeField] public bool hasBacksight;
+
+    //  Checkers
+    [Header("Checkers")]
+    [SerializeField] public Transform lineOfSight;
+    [SerializeField] private Transform wallChecker;
+    [SerializeField] private Transform ledgeChecker;
+
     //cached values
+    [Header("Cached Values")]
     public Rigidbody2D rb;
     public Animator anim;
     public float distanceRay = 5;
 
     void Awake()
     {
-        mainPlayer = GameManager.instance.PlayerStats.GetSetPlayerCharacterObj.transform.parent.gameObject;
+        //mainPlayer = GameManager.instance.PlayerStats.GetSetPlayerCharacterObj.transform.parent.gameObject;
 
         isPatrolling = true;
+        isAgro = false;
         isChasing = false;
-        isAttacking = false;
+        isSteppingOnLedge = false;
+        isFacingRight = true;
         startingPoint = gameObject.transform.position;
         Debug.Log("first starting point:" + startingPoint);
         chaseThreshold = 15f;
@@ -42,30 +59,74 @@ public class AIPatrol : MonoBehaviour
     //  Processes whether state is climbing or patrolling
     private void Update()
     {
-        //  Enemy is close enough for player to chase
-        if (Vector3.Distance(transform.position, mainPlayer.transform.position) <= chaseThreshold)
+        if (willChasePlayer)
         {
-            isChasing = true;
-        }
-        //  Enemy is too far for player to chase
-        else
-        {
-            //  If the player got away, the enemy will have a new starting point to patrol from
-            if (isChasing)
+            //  Enemy is close enough for player to chase
+            //if (Vector2.Distance(transform.position, mainPlayer.transform.position) <= chaseThreshold)
+            if (CanSeePlayer(chaseThreshold))
             {
-                startingPoint = transform.position;
-                Debug.Log("new starting point:" + startingPoint);
-                isChasing = false;
-                
+                isPatrolling = false;
+                isAgro = true;
             }
-            //  enemy will stop chasing the player and go back to patrolling based on the previously created starting point
+            //  Enemy cannot see player in general
             else
             {
-                if (anim.GetBool("isClimbing"))
-                    isPatrolling = false;
+                //  If the enemy cannot see the player because they got away, the enemy wait 2 seconds then will have a new starting point to patrol from
+                if (isAgro)
+                {
+                    if (!isChasing)
+                    {
+                        isChasing = true;
+                        Invoke("StopChasingPlayer", 5);
+                    }
+                }
+
+                //  enemy will stop chasing the player and go back to patrolling based on the previously created starting point
                 else
-                    isPatrolling = true;
+                {
+                    if (anim.GetBool("isClimbing"))
+                        isPatrolling = false;
+                    else
+                        isPatrolling = true;
+                }
             }
+
+            if (isAgro)
+            {
+                ChasePlayer();
+            }
+        }
+
+        //  Processes what to do if the enemy is touching a wall
+        if(HasTouchedWall(0.125f))
+        {
+            if (hasClimbingFeature)
+            {
+                //If not yet climbing, stop patrolling and play the climbing animation
+                if (!anim.GetBool("isClimbing"))
+                {
+                    isPatrolling = false;
+                    anim.SetBool("isClimbing", true);                   
+                }                    
+
+                //  We separate this loop so that the enemy will climb on EVERY FRAME that isClimbing is True
+                if (anim.GetBool("isClimbing"))
+                {
+                    ClimbUp();                  
+                }
+            }
+            else
+            {
+                Flip();
+            }
+        }
+
+        else if (HasDetectedLedge(0.125f) && anim.GetBool("isClimbing"))
+        {
+            anim.SetBool("isClimbing", false);
+            isSteppingOnLedge = false;
+            isPatrolling = true;
+            rb.gravityScale = 1f;
         }
     }
 
@@ -73,7 +134,98 @@ public class AIPatrol : MonoBehaviour
     {      
         //  Decide whether to patrol or to chase
         if (isPatrolling) Patrol();
-        if (isChasing) ChasePlayer();      
+    }
+
+    /*private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (hasClimbingFeature)
+        {
+            if (collision.gameObject.tag == "Ground" && anim.GetBool("isClimbing"))
+            {
+                Debug.Log("back to walking");
+                anim.SetBool("isClimbing", false);
+                isPatrolling = true;
+                rb.gravityScale = 1f;
+            }
+
+        }
+    }*/
+
+    private bool CanSeePlayer(float aggroThreshold)
+    {
+        bool val = false;
+        float sightRange = aggroThreshold;
+
+        if (!isFacingRight)
+        {
+            sightRange = -aggroThreshold;
+        }
+
+        Vector2 endPos = lineOfSight.position + Vector3.right * sightRange;
+        RaycastHit2D playerHit = Physics2D.Linecast(lineOfSight.position, endPos, LayerMask.GetMask("Character"));
+        
+
+        if (playerHit.collider != null)
+        {
+            if (playerHit.collider.gameObject.CompareTag("Player"))
+            {
+                detectedPlayer = playerHit.collider.gameObject;
+                val = true;
+            }
+            else
+                val = false;
+        }
+        else
+        {
+            Debug.DrawLine(lineOfSight.position, endPos, Color.yellow);
+        }
+        return val;
+    }
+
+    private bool HasTouchedWall(float wallSearchThreshold)
+    {
+        float wallRange = wallSearchThreshold;
+
+        if (!isFacingRight)
+        {
+            wallRange = -wallSearchThreshold;
+        }
+        Vector2 wallRangeEnd = wallChecker.position + Vector3.right * wallRange;
+        RaycastHit2D wallFinder = Physics2D.Linecast(wallChecker.position, wallRangeEnd, LayerMask.GetMask("Ground"));
+        if (wallFinder.collider != null)
+        {
+            isTouchingWall = true;
+        }
+        else
+        {
+            isTouchingWall = false;
+        }
+        Debug.DrawLine(wallChecker.position, wallRangeEnd, Color.yellow);
+        return isTouchingWall;
+    }
+
+    private bool HasDetectedLedge(float ledgeSearchThreshold)
+    {
+
+        float ledgeRange = ledgeSearchThreshold;
+
+        if (!isFacingRight)
+        {
+            ledgeRange = -ledgeSearchThreshold;
+        }
+        Vector2 ledgeRangeEnd = ledgeChecker.position + Vector3.right * ledgeRange;
+        RaycastHit2D ledgeFinder = Physics2D.Linecast(ledgeChecker.position, ledgeRangeEnd, LayerMask.GetMask("Ground"));
+        if (ledgeFinder.collider != null)
+        {
+            isSteppingOnLedge = false;
+            Debug.Log("looking for ledge");
+        }
+        else
+        {
+            isSteppingOnLedge = true;
+        }
+        Debug.DrawLine(ledgeChecker.position, ledgeRangeEnd, Color.green);
+        return isSteppingOnLedge;
     }
 
     private void Patrol()
@@ -91,7 +243,8 @@ public class AIPatrol : MonoBehaviour
     {
         isPatrolling = false;
         transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-        patrolSpeed *= -1;      
+        patrolSpeed *= -1;
+        isFacingRight = !isFacingRight;
     }
 
     public void ClimbUp()
@@ -102,22 +255,23 @@ public class AIPatrol : MonoBehaviour
     }
 
     private void ChasePlayer()
-    {
-        isPatrolling = false;
+    {       
         Debug.Log("is chasing the player");
+        //To do: add secondary raycast to check if player is behind enemy WHILE in chase state
+
         //  enemy is to the left of player
-        if (transform.position.x < mainPlayer.transform.position.x && patrolSpeed < 0 && mainPlayer.GetComponent<GroundPlayerController>().CheckIfTouchGround) Flip();
+        if (transform.position.x < detectedPlayer.transform.position.x && patrolSpeed < 0 && detectedPlayer.GetComponent<GroundPlayerController>().CheckIfTouchGround) Flip();
         //  enemy is to the right of player
-        else if (transform.position.x > mainPlayer.transform.position.x && patrolSpeed > 0 && mainPlayer.GetComponent<GroundPlayerController>().CheckIfTouchGround) Flip();
+        else if (transform.position.x > detectedPlayer.transform.position.x && patrolSpeed > 0 && detectedPlayer.GetComponent<GroundPlayerController>().CheckIfTouchGround) Flip();
 
         rb.velocity = new Vector2(patrolSpeed * 2 * Time.fixedDeltaTime, 0f);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void StopChasingPlayer()
     {
-        if (collision.gameObject.tag == "Player")
-        {
-            isAttacking = false;
-        }
+        isAgro = false;
+        isChasing = false;
+        startingPoint = transform.position;
+        isPatrolling = true;
     }
 }
