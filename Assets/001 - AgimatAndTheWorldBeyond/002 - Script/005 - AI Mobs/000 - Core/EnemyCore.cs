5c162053e2e3c17f8c4d2fea7cf34b128f8b2bfd
paoloandal <paoloa@realtair.com>
2021-08-30 15:20:44 +0800

    Created new files for events
    individual states to follow

---
+++ Assets/001 - AgimatAndTheWorldBeyond/002 - Script/005 - AI Mobs/000 - Core/EnemyCore.cs
@@ -0,0 +1,26 @@
+﻿using MyBox;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCore : MonoBehaviour
+{
+    [Header("SETTINGS")]
+    public EnemyMobData enemyMobData;
+    public Rigidbody2D enemyRB;
+    public Collider2D enemyBodyCollider;
+    public Collider2D enemyFeetOffsetCollider;
+    public Transform mainEnemyMob;
+    public Transform enemyEnvCheckerXRot;
+
+    [Header("DEBUGGER")]
+    [ReadOnly] public Vector2 GetCurrentVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CurrentVelocitySetter() => GetCurrentVelocity = enemyRB.velocity;
+}
---
+++ Assets/001 - AgimatAndTheWorldBeyond/002 - Script/005 - AI Mobs/002 - StateMachines/EnemyStatesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatesController
+{
+    protected EnemyMobData movementData;
+
+    protected float startTime;
+    protected float lastChangeWeaponTime;
+    protected float lastLedgeClimb;
+
+    protected bool isAnimationFinished;
+    protected bool isExitingState;
+
+    public string animBoolName;
+
+    /* TO DO:
+     * Add proper cs file to instantiate at GameManager.cs
+     */
+    public virtual void Enter()
+    {
+        DoChecks();
+        //GameManager.instance.PlayerStats.GetSetPlayerAnimator.SetBool(animBoolName, true);
+        startTime = Time.time;
+        isAnimationFinished = false;
+        isExitingState = false;
+    }
+
+    public virtual void Exit()
+    {
+        //GameManager.instance.PlayerStats.GetSetPlayerAnimator.SetBool(animBoolName, false);
+        isExitingState = true;
+    }
+
+    public virtual void LogicUpdate()
+    {
+        DoChecks();
+    }
+
+    public virtual void PhysicsUpdate() { }
+
+    public virtual void DoChecks() { }
+
+    public virtual void AnimationTrigger() { }
+
+    public virtual void AnimationFinishTrigger() => isAnimationFinished = true;
+}
